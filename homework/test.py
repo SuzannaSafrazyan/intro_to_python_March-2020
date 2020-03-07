b28ee37bd4559a4d6875a2c6d8aba2b71b114356
SuzannaSafrazyan <61898269+SuzannaSafrazyan@users.noreply.github.com>
2020-03-07 13:21:24 +0400

    Update test.py

--- homework/test.py
+++ homework/test.py
@@ -1,1 +1,66 @@
 hi
+
+
+ACA Student 011@DESKTOP-6M5S9OO MINGW64 ~ (fghgjhk)
+$ pwd
+/c/Users/stude
+
+ACA Student 011@DESKTOP-6M5S9OO MINGW64 ~ (fghgjhk)
+$ cd Desktop/
+
+ACA Student 011@DESKTOP-6M5S9OO MINGW64 ~/Desktop (fghgjhk)
+$ ls
+ __MACOSX/                   dres.webp                     Lecture1/          mockup.jpg                  red-dress.jpg               Vahan/
+ Atom.lnk*                  'Eclipse Java Neon.lnk'*       Lecture2/          mockups.png                 red-dress-mini.webp        'Visual Studio Code.lnk'*
+'Balsamiq Wireframes.lnk'*   finalexam/                    Lecture2.zip      'New Project 2.bmpr'         SocialNetwork/             'Wires - Mobile.xd'
+ class/                      get-pip.py*                   Lecture3/          Online_Shop/                style.css
+ Desktop/                    intro_to_python_March-2020/   Lecture3.zip       Postman.lnk*                test.txt
+ desktop.ini                 koduxaki.txt                  Lesson3.pptx       presentation_2x.jpg         Tigran_Movsisyan_Exam.zip
+ dr.jpg                      lect/                        'mini dress.webp'   react-update-react-empty/   Untitled.ipynb
+
+ACA Student 011@DESKTOP-6M5S9OO MINGW64 ~/Desktop (fghgjhk)
+$ cd intro_to_python_March-2020/
+
+ACA Student 011@DESKTOP-6M5S9OO MINGW64 ~/Desktop/intro_to_python_March-2020 (master)
+$ git add .
+warning: LF will be replaced by CRLF in homework/.ipynb_checkpoints/practical 1 march 7 2020-checkpoint.ipynb.
+The file will have its original line endings in your working directory
+warning: LF will be replaced by CRLF in homework/practical 1 march 7 2020.ipynb.
+The file will have its original line endings in your working directory
+warning: LF will be replaced by CRLF in homework/problem4.py.
+The file will have its original line endings in your working directory
+
+ACA Student 011@DESKTOP-6M5S9OO MINGW64 ~/Desktop/intro_to_python_March-2020 (master)
+$ git commit -m "practical"                                                                                                                                            [master 6cad4a9] practical
+ 4 files changed, 62 insertions(+)
+ create mode 100644 homework/.ipynb_checkpoints/practical 1 march 7 2020-checkpoint.ipynb
+ create mode 100644 homework/practical 1 march 7 2020.ipynb
+ create mode 100644 homework/problem4.py
+ rename homework/{test.txt => test.py} (100%)
+
+ACA Student 011@DESKTOP-6M5S9OO MINGW64 ~/Desktop/intro_to_python_March-2020 (master)
+$ git push
+remote: Permission to SuzannaSafrazyan/intro_to_python_March-2020.git denied to AregPapyan.
+fatal: unable to access 'https://github.com/SuzannaSafrazyan/intro_to_python_March-2020.git/': The requested URL returned error: 403
+
+ACA Student 011@DESKTOP-6M5S9OO MINGW64 ~/Desktop/intro_to_python_March-2020 (master)
+$ git push
+Enumerating objects: 9, done.
+Counting objects: 100% (9/9), done.
+Delta compression using up to 4 threads
+Compressing objects: 100% (7/7), done.
+Writing objects: 100% (7/7), 1.04 KiB | 533.00 KiB/s, done.
+Total 7 (delta 0), reused 0 (delta 0)
+To https://github.com/SuzannaSafrazyan/intro_to_python_March-2020.git
+   e15523f..6cad4a9  master -> master
+
+ACA Student 011@DESKTOP-6M5S9OO MINGW64 ~/Desktop/intro_to_python_March-2020 (master)
+$ pwd
+/c/Users/stude/Desktop/intro_to_python_March-2020
+
+ACA Student 011@DESKTOP-6M5S9OO MINGW64 ~/Desktop/intro_to_python_March-2020 (master)
+$ ls
+homework/  README.md
+
+ACA Student 011@DESKTOP-6M5S9OO MINGW64 ~/Desktop/intro_to_python_March-2020 (master)
+$
